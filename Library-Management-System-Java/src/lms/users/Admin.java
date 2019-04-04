4022043213a7a417c9c9cdb10ec12661108f5436
Samir Anghan <samir.anghan@gmail.com>
2019-04-03 21:41:41 -0400

    [Admin] Added missing method stubs.

--- Library-Management-System-Java/src/lms/users/Admin.java
+++ Library-Management-System-Java/src/lms/users/Admin.java
@@ -3,6 +3,8 @@
  */
 package lms.users;
 
+import lms.books.Book;
+
 /**
  * @author Samir
  *
@@ -13,6 +15,40 @@ public class Admin {
 		
 	}
 	
+	public void updateUser(User user) {
+		
+	}
 	
+	public void deleteUser(User user) {
+		
+	}
+	
+	public void showAllUsers() {
+		
+	}
+	
+	public void searchUser(User user) {
+		
+	}
+	
+	public void addBook(Book book) {
+		
+	}
+	
+	public void deleteBook(Book book) {
+		
+	}
+	
+	public void modifyBook(Book book) {
+		
+	}
+	
+	public void showBorrowedBooks() {
+		
+	}
+	
+	public void showReturnedBooks() {
+		
+	}
 
 }
