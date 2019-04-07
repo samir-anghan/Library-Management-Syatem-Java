51129bd4bd248bc33b162ee2b6220f40a7e905a4
Samir Anghan <samir.anghan@gmail.com>
2019-04-06 20:54:26 -0400

    [Penalty] Implemented methods.

--- Library-Management-System-Java/src/lms/users/Penalty.java
+++ Library-Management-System-Java/src/lms/users/Penalty.java
@@ -4,6 +4,10 @@
 package lms.users;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import lms.books.Book;
+import lms.books.LoanedBook;
 
 /**
  * @author Samir
@@ -13,6 +17,9 @@ public class Penalty {
 	
 	private int receipt_no;
 	private Date date;
+	
+	User borrower = User.getInstance();
+	LoanedBook book = new LoanedBook();
 	
 	public int getReceipt_no() {
 		return receipt_no;
@@ -31,10 +38,25 @@ public class Penalty {
 	}
 
 	public void calculatePenalty() {
-		
+		 int daysOverdue;
+         double overdueFine;
+         Date today = new Date();
+         Date bookReturnDate = book.getReturnDate();
+         // Calculates days between today and the book return date
+         //Today is the date when borrower is actually returning the book
+         //Return date is the date when borrower supposed to return a book
+         long diff = today.getTime() - bookReturnDate.getTime();
+         daysOverdue = (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+         
+         // Checks if there are any actual days overdue, and apply fine accordingly
+         if (daysOverdue > 0) {
+             	overdueFine = daysOverdue * 5;
+             	borrower.setPenaltyDueAmount(borrower.getPenaltyDueAmount() + overdueFine);
+         }
+
 	}
 
 	public void addPenalty(double penalty) {
-		
+		borrower.setPenaltyDueAmount(borrower.getPenaltyDueAmount() + penalty);
 	}
 }
